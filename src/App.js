050ed89b6796d83fa6e9d9230cbaebcaf2429ff3
Maya Imhoff <mayelyimhoff@gmail.com>
2021-04-12 13:37:55 +0200

    all works now

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import React, { useState, useEffect } from "react";
 import QuoteCard from './components/QuoteCard';
 import './App.css';
 
+
+const defaultQuote = {
+  quote: "Oh Yeah!",
+  character: "Duffman",
+  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FDuffman.png?1497567511709",
+  characterDirection: "Left"
+};
+
 function App() {
+  const [quote, setQuote] = useState(defaultQuote);
+
+  
+  const simpsonsQuote = () => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=num')
+      .then((res) => res.data)
+      .then((data) => setQuote(data[0]));
+  };
+
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <QuoteCard quote={quote} />
+    <button type="button" onClick={simpsonsQuote}>Gimme yummy Simpsons quote</button>
+    </>
   );
 }
 
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './QuoteCard.css';
 
-function QuoteCard() {
+function QuoteCard({ quote }) {
     return (
-        <div>
+        quote && (
+        <div className='QuoteCard'>
+            <img src={quote.image} alt={quote.character} />
+            <h1>{quote.character} says while looking to the {quote.characterDirection} side:</h1>
+            <h2>{quote.quote}</h2>
             
         </div>
+        )
     )
 }
 
@@ -15,3 +20,10 @@ export default QuoteCard
 // character: who said the quote
 // image: the character's image
 // characterDirection: direction the character faces (left or right)
+
+
+// The application should include: A button to retrieve a quote, an area where the name of the character, the photo and the quote is displayed (it is possible to reuse code from previous quests, namely the QuoteCard component)
+
+// function myFunc({name, age}) {
+
+// }
